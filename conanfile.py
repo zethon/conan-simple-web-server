e05da1025d570c2bd7478bdec1c0b22a295d946a
a_teammate <madoe3@web.de>
2019-03-08 02:18:23 +0100

    down-grade to header-only and upgrade test_package

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 import os
 
 
@@ -15,58 +15,28 @@ class LibnameConan(ConanFile):
     homepage = "https://gitlab.com/eidheim/Simple-Web-Server"
     author = "Ole Christian Eidheim <eidheim@gmail.com>"
     license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    no_copy_source = True
 
-    # Options may need to change depending on the packaged library.
-    settings = "os", "arch", "compiler", "build_type"
-    options = {}
-    default_options = {}
-    #options = {"shared": [True, False], "fPIC": [True, False]}
-    #default_options = {"shared": False, "fPIC": True}
+    # Packages the license for the conanfile.py
+    exports = ["LICENSE.md"]
 
     # Custom attributes for Bincrafters recipe conventions
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    _source_subfolder = "Simple-Web-Server"
 
     requires = (
         "asio/1.12.2@bincrafters/stable"
     )
 
-    def config_options(self):
-        pass
-
     def source(self):
         git = tools.Git(self._source_subfolder)
         git.clone("https://gitlab.com/eidheim/Simple-Web-Server.git")
         git.checkout("v3.0.0-rc3")
-        
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"  # example
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
 
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.configure()
-        cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="*.hpp", dst="include", src=self._source_subfolder)
 
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+
+    def package_id(self):
+        self.info.header_only()
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -16,5 +15,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
